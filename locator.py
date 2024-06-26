5a59fd8d86cc0eb9d019701538f7adc3cfdb2097
Kyra15-CP <kmovva@carepredict.com>
2024-06-26 00:47:47 -0400

    Update locator.py

--- locator.py
+++ locator.py
@@ -1,23 +1,23 @@
-#import necessary packages
+# import necessary packages
 from csvfunctions import *
 
 combined_to_file()
-#find the path of the excel file from desktop
+# find the path of the excel file from desktop
 csv_file = "active_sites.csv"
-#read the the excel file
+# read the the excel file
 df = pd.read_csv(csv_file)
 
-#get rid of whitespace from column names
+# get rid of whitespace from column names
 df.columns = df.columns.str.strip()
 
-#accessing columns
+# accessing columns
 column1_data = df["facility_id"]
 column2_data = df["facility_name"]
 column3_data = df["institution_id"]
 column4_data = df["institution_name"]
 
 
-#define the match function
+# define the match function
 def match_institution_id(facility_id):
     filtered_df = df[df['facility_id'] == facility_id]
     institution_id = filtered_df.iloc[:, -2:-1].values[0]
