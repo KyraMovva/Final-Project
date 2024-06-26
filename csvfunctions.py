48ccc3c09ba11c4ccdc1fa864f49ee10aabae922
Kyra15-CP <kmovva@carepredict.com>
2024-06-26 00:47:23 -0400

    Update csvfunctions.py

--- csvfunctions.py
+++ csvfunctions.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from functools import *
 
+
+# Pandas display options
 display = pd.options.display
 display.max_columns = 6
 display.max_rows = 10
 display.max_colwidth = 222
 display.width = None
 
+# Reading various csvs given
 job_table = pd.read_csv("job.csv")
 
 tempo_worn = pd.read_csv("tempo_worn.csv")
@@ -15,14 +18,11 @@ facility = pd.read_csv("facility.csv")
 
 job_table_spliced = (job_table[['facility_id', "cron_expression", "file_name", "recurrence", "local_start_at"]])
 job_table_spliced = job_table_spliced[job_table_spliced['recurrence'].astype(str).str.contains("1")]
-job_table_spliced.to_csv("job_table_spliced.csv", sep=',', index=False, encoding='utf-8')
 
 
 tempo_worn_spliced = tempo_worn[["facility_id", "institution_id", "rr_rule", "start_date", "report_file_name"]]
-tempo_worn_spliced.to_csv("tempo.csv", sep=',', index=False, encoding='utf-8')
 
 active_facilities = facility[["id", "facility_name", "institution_id"]].rename(columns={"id": "facility_id"})
-
 
 inst_table = pd.read_csv("institution.csv")
 
@@ -31,15 +31,12 @@ active_sites = pd.merge(active_facilities, inst_spliced, how='left')
 
 sorted_tab = active_sites.sort_values("institution_id")
 
-sorted_tab.to_csv("active_sites.csv", sep=',', index=False, encoding='utf-8')
 
-
+# Merges to make a full list of all active facilities with their cron and rrule expressions
 def merge_csvs():
     active_jobs = pd.merge(sorted_tab, job_table_spliced, how="left")
-    active_jobs.to_csv("active_jobs.csv", sep=',', index=False, encoding='utf-8')
 
     active_tempos = pd.merge(sorted_tab, tempo_worn_spliced, how="left")
-    active_tempos.to_csv("active_tempos.csv", sep=',', index=False, encoding='utf-8')
 
     full_list = pd.concat([active_tempos, active_jobs], ignore_index=True)
     full_list = full_list.sort_values("facility_id")
@@ -49,22 +46,15 @@ def merge_csvs():
 
 # Returns a set with the unique report names
 def create_unique_report_names():
-    job_table = pd.read_csv("job.csv")
-
-    tempo_worn = pd.read_csv("tempo_worn.csv")
-
-    job_table_spliced = job_table[['facility_id', "file_name", "cron_expression"]]
-
-    tempo_worn_spliced = tempo_worn[["facility_id", "institution_id", "rr_rule", "report_file_name"]]
-
-    unique_report_names = job_table_spliced['file_name'].to_string(index=False).split("\n")
-    uni = tempo_worn_spliced["report_file_name"].to_string(index=False).split("\n")
-    unique_report_names.extend(uni)
+    # unique_report_names = job_table_spliced['file_name'].to_string(index=False).split("\n")
+    # uni = tempo_worn_spliced["report_file_name"].to_string(index=False).split("\n")
+    # unique_report_names.extend(uni)
+    unique_report_names = pd.read_csv("report_names.csv")["Alert Reports:"].tolist()
     for x in unique_report_names:
-        unique_report_names[unique_report_names.index(x)] = x.strip().replace(".pdf", "").title().replace("_", " ")
+        unique_report_names[unique_report_names.index(x)] = x.strip().replace(".pdf", "").replace("_", " ").title()
     unique_report_names = reduce(lambda re, x: re+[x] if x not in re else re, unique_report_names, [])
     with open("report_names.csv", "w") as f:
-        f.write('Alert Reports!!\n')
+        f.write('Alert Reports:\n')
         for items in unique_report_names:
             f.write('%s\n' % items)
 
@@ -106,4 +96,3 @@ def active_facility_id_name():
         final_list.append(id_list[x] + " - " + name_list[x])
 
     return final_list
-
